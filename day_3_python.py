a46f42ad691261e600d57ddbcc3183d634e80cc2
tanapolark <117499712+tanapolark@users.noreply.github.com>
2023-09-10 09:24:04 +0700

    Add files via upload

---
+++ day_3_python.py
@@ -0,0 +1,9 @@
+print("Hello!")
+
+food = input("Name a food: ")
+plant = input("Name a plant: ")
+cooking = input("Name a method of cooking: ")
+burned = input("How would you describe burned food?: ")
+item = input("Name a household item: ")
+
+print(cooking, food, "with", burned, plant, "on a", item)
