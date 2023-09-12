f1a98a84b7dfb3c22bd3f719250a50627e3fc496
tanapolark <117499712+tanapolark@users.noreply.github.com>
2023-09-12 20:33:06 +0700

    Add files via upload

---
+++ day_5_if_statements.py
@@ -0,0 +1,18 @@
+print("Marvel Movie Character Creator")
+print("----------------------")
+
+answer1 = input("Do you like 'hanging around'?: ")
+
+if answer1 == "Yes":
+  print("You're Spider-man!")
+else:
+  print("You're not Spider-man!")
+  answer2 = input("Do you have a shield?: ")
+
+  if answer2 == "Yes":
+    print("You're Captain America!")
+  else:
+    print("You're not Captain America!")
+      
+    
+
