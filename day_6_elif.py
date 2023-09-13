6b09678e53f3e73e72c7d5867aa428cdb19697ba
tanapolark <117499712+tanapolark@users.noreply.github.com>
2023-09-13 09:50:00 +0700

    Add files via upload
    Day 6 of coding in Python challenge 
    
    Today's lesson is elif statement

---
+++ day_6_elif.py
@@ -0,0 +1,20 @@
+print("Login System")
+print("-----------------")
+
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "plumeinwZA007" and password == "bestasuraTH":
+  print("Welcome Plume, come in to cook some meth again huh?")
+
+elif username == "NeooreoGunkeeper" and password == "smokeweedeveryday420":
+  print("You should eat food sometime, if not Plume got your meth go get it")
+
+elif username == "Kopkohellokitty" and password == "stopeatmyassZomboid":
+  print("No, they won't stop eat your ass")
+
+elif username == "gamekyMILFslayer69" and password == "pekoramylovefafafafa":
+  print("Well well, isn't this Westpoint biggest phanthom thief of all time")
+
+else:
+  print("Who the hell are you?")
---
+++ day_6_fixing_code.py
@@ -0,0 +1,12 @@
+season = input("what is your favorite season?")
+
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
