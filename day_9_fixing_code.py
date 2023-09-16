dd717b91cb4d71214b7677222e7526de32af0519
tanapolark <117499712+tanapolark@users.noreply.github.com>
2023-09-16 09:12:38 +0700

    Add files via upload

---
+++ day_9_fixing_code.py
@@ -0,0 +1,8 @@
+score = int(input("What was your score on the test?"))
+
+if score >= 80:
+  print("Not too shabby")
+elif score > 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
