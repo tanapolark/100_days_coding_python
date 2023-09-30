74b80e25bdc70c4072abf152922251135e313a04
tanapolark <117499712+tanapolark@users.noreply.github.com>
2023-09-30 11:46:43 +0700

    Add files via upload

---
+++ day_23_def.py
@@ -0,0 +1,12 @@
+def login():
+  print("Login System")
+  username = input("What is your username?: ")
+  password = input("What is your password?: ")
+    
+  if username == "teteINWza007" and password == "omgomgBIGCo":
+    print("Welcome Tete!")
+  else:
+    print("Whoops! I don't recognize that username or password. Try again!")
+
+login()
+      
